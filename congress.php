568b11a0e4c65d3e2bd3d9f5f1dc7bdcd4dbbe70
Anush Kadoyan <anushkadoyan@gmail.com>
2016-10-16 01:58:56 -0700

    Added data persistance after Search click

--- congress.php
+++ congress.php
@@ -10,7 +10,10 @@
 		<meta charset="utf-8"/>
 	</head>
 	
-	<!--     API Key: 9d713eee2bda4febb053035ef76e5f4c -->
+	<!--     API Key: 9d713eee2bda4febb053035ef76e5f4c 
+		https://congress.api.sunlightfoundation.com/[method]
+		/legislators?apikey=[your_api_key]
+	-->
 		<script>
 
 		function selectChange() {
@@ -38,7 +41,11 @@
 		function clearClicked() {
 			
 			var keySelect = document.getElementById("keyword-title");
+			var keyInput = document.getElementById("keyword-input");
+
 			keySelect.innerHTML = "Keyword*";
+			keyInput.value = "";
+
 
 		}
 		
@@ -61,7 +68,6 @@
 				}
 				alert(text);
 			}
-			return false;
 		}
 	</script>
 	<body>
@@ -71,8 +77,8 @@
 				<form id="myForm" action="" method="post">
 					<div id="congress-selection">
 						<div style="display: inline-block;" id="congress-title">Congress Database</div>
-						<select id="con-select" style="" name="Select your option" onchange="selectChange()">
-							<option value="default" selected="selected" disabled>Select your option</option>
+						<select id="con-select" style="" name="selectOption" onchange="selectChange()">
+							<option value="default" selected=selected disabled>Select your option</option>
 							<option value="Legislators">Legislators</option>
 							<option value="Committees">Committees</option>
 							<option value="Bills">Bills</option>
@@ -89,7 +95,7 @@
 					<div id="keyword-selection">
 						<input style="display: none"  id="secret" name="keyword-title" value="">
 						<div style="display: inline-block;"  id="keyword-title">Keyword*</div>
-						<input type="text" id="keyword-input" value="<?php if(isset($_POST['keyword'])) { echo htmlentities ($_POST['keyword']); }?>" name="keyword" style="width: 130px;"><br>
+						<input type="text" id="keyword-input"  name="keyword" style="width: 130px;"></br>
 					</div>
 					<div id="search-clear" style="width: 100%;">
 						<input type="submit" form="myForm" value="Search" onclick="searchClicked()" name="formSubmit" ><br>
@@ -106,31 +112,44 @@
 		
 		<div>
 			<?php
-				if(isset($_POST['keyword'])) {				
+				//data persists after form submit
+				if(isset($_POST['keyword'])) {	
+					?><script>
+					document.getElementById("keyword-input").value = "<?php echo $_POST['keyword'];?>";</script>
+					<?php
 				}
 				if(isset($_POST['keyword-title'])) {	
 
 				?>
-				<script>
+					<script>
 					document.getElementById("keyword-title").innerHTML = "<?php echo $_POST['keyword-title'];?>";</script>
 				<?php 
-					
-				if(!isset($_POST['keyword-title']) || !$_POST['keyword-title']) {
-					
-					?>
-					
-				<script>
+				}
+				if(!isset($_POST['keyword-title']) || !$_POST['keyword-title']) {?>
+					<script>
 					document.getElementById("keyword-title").innerHTML = "Keyword*";</script>
-	
 					<?php
 				}
-					
-					
-				?>
-			<?php
-				}
+
 				if(isset($_POST['Senate_House'])) {	
+					?>
+					<script>
+					document.getElementsByName("Senate_House").value = "<?php echo $_POST['Senate_House'];?>";</script>
+					<?php
 				}
+				if(isset($_POST['selectOption'])) {
+					$select = $_POST['selectOption'];
+					?>
+					
+					<script>
+					document.getElementById("con-select").value="<?php echo $_POST['selectOption']?>";</script>
+					<?php
+				}
+				
+							
+				
+				foreach ($_POST as $key => $value)
+ echo "Field ".htmlspecialchars($key)." is ".htmlspecialchars($value)."<br>";
 			?>
 		</div>
 		
